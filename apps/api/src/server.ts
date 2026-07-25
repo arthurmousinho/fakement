0aa172c22ab0c70f0dc9c9861584f2227ab66e1f
arthurmousinho <arthursilvamo296@gmail.com>
2026-07-25 10:19:27 -0300

    feat: add server error handler for zod errors

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,9 +1,30 @@
+import { ZodError } from "zod";
 import { createApp } from "./app.ts";
 import { apiKeyRoutes } from "./app/routes/api-key.routes.ts";
 import { env } from "./config/env.ts";
 
 export const appSingleton = createApp();
 
+// Errors
+appSingleton.setErrorHandler((error, request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({
+      error: "BAD_REQUEST",
+      code: 400,
+      message: error.issues.map((issue) => issue.message),
+    });
+  }
+
+  request.log.error(error);
+
+  return reply.status(500).send({
+    statusCode: 500,
+    error: "Internal Server Error",
+    message: "Unexpected error",
+  });
+});
+
+// Routes
 appSingleton.register(apiKeyRoutes, { prefix: "/api-keys" });
 
 try {
