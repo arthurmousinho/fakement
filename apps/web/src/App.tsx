09c62e6c63fc476fe5a98f5e6f80082f2b0c64c2
arthurmousinho <arthursilvamo296@gmail.com>
2026-07-28 14:28:18 -0300

    fix: shadcn installation

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,3 +1,10 @@
+import { Button } from "./components/ui/button";
+
 export function App() {
-  return <h1>Fakement</h1>;
+  return (
+    <div>
+      <h1>Fakement</h1>
+      <Button>Pay</Button>
+    </div>
+  );
 }
