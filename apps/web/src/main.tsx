d5dc6f89270043c555dfcfa53e0e79b6e7967bfa
arthurmousinho <arthursilvamo296@gmail.com>
2026-07-28 11:19:44 -0300

    feat: add ui theme settings

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,9 @@
+import "@fontsource-variable/ibm-plex-sans/wght.css";
+import "./index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
-import "./index.css";
 import { App } from "./App.tsx";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 
