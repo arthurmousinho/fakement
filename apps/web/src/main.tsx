e7afad21c13c768dbfa48c07c61ac7fcfe31046e
arthurmousinho <arthursilvamo296@gmail.com>
2026-07-29 09:43:54 -0300

    chore: add react query setup

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -6,11 +6,15 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./lib/router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/query-client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <ThemeProvider>
-      <RouterProvider router={router} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </StrictMode>,
 );
