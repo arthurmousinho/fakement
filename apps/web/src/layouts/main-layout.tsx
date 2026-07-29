5bec12fe9ac53c3d0ea4abab523ceb9a93ff6fdb
arthurmousinho <arthursilvamo296@gmail.com>
2026-07-29 16:05:33 -0300

    feat: improve web ui layout

--- apps/web/src/layouts/main-layout.tsx
+++ apps/web/src/layouts/main-layout.tsx
@@ -4,9 +4,11 @@ import { Outlet } from "react-router-dom";
 export function MainLayout() {
   return (
     <div>
-      <Header />
-      <div className="mt-4 px-6 py-2">
-        <Outlet />
+      <div className="max-w-[80%] min-h-250 mx-auto p-4">
+        <Header />
+        <div className="mt-4 px-6 py-2">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
